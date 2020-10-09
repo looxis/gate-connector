a843be5888fa240d0bd727b5f01e9956badc2581
Chris Stefener <cstefener@gmail.com>
2020-10-09 12:26:55 +0200

    Add Laravel 8 support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 All notable changes to `gate` will be documented in this file
 
+## 2.0.0 - 2020-10-08
+- Add Laravel 8 Support
+
 ## 1.0.0 - 201X-XX-XX
 
 - initial release
--- src/LoginControllerTrait.php
+++ src/LoginControllerTrait.php
@@ -7,7 +7,7 @@ use Auth;
 use Redirect;
 use Exception;
 use App\Http\Controllers\Controller;
-use App\User;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
