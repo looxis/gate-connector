67cdcafdeed9c65fbbf592cdb0288f2997383256
Jannik Malken <mannikj@web.de>
2024-02-12 10:29:27 +0000

    Upgrade dependencies

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get(config('gate.auth_callback_uri', 'auth/callback'), config('gate.auth_callback_controller', 'App\Http\Controllers\Auth\LoginController') . '@handleProviderCallback');
+Route::get(
+    config('gate.auth_callback_uri', 'auth/callback'),
+    config('gate.auth_callback_controller', 'App\Http\Controllers\Auth\LoginController') . '@handleProviderCallback'
+);
